d08e435abfea4e61186c0088a20858dc4b1f4847
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-30 03:26:40 +0400

    Feat better executor service by using virtual threads

--- src/main/java/com/hadzhy/jetquerious/asynch/JetQExecutor.java
+++ src/main/java/com/hadzhy/jetquerious/asynch/JetQExecutor.java
@@ -1,30 +1,27 @@
 package com.hadzhy.jetquerious.asynch;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.locks.LockSupport;
 import java.util.function.Supplier;
 import com.hadzhy.jetquerious.ds.JetMPSC;
 
 public final class JetQExecutor {
-  private final ExecutorService executor;
   private final JetMPSC<TaskWrapper<?>> queue;
   private volatile boolean shutdown = false;
 
   private static final int DEFAULT_QUEUE_SIZE_POWER = 16;
-  private static final int DEFAULT_WORKER_THREADS = Runtime.getRuntime().availableProcessors() * 2;
 
   public JetQExecutor() {
-    this(DEFAULT_QUEUE_SIZE_POWER, DEFAULT_WORKER_THREADS);
+    this(DEFAULT_QUEUE_SIZE_POWER);
   }
 
-  public JetQExecutor(int queueSizePowerOfTwo, int workerThreads) {
-    validateParameters(queueSizePowerOfTwo, workerThreads);
+  public JetQExecutor(int queueSizePowerOfTwo) {
+    if (queueSizePowerOfTwo < 0)
+      throw new IllegalArgumentException("Queue size power cannot be negative");
+    if (queueSizePowerOfTwo > 32)
+      throw new IllegalArgumentException("Queue size power too large (max 32)");
 
     this.queue = new JetMPSC<>(queueSizePowerOfTwo);
-    this.executor = Executors.newFixedThreadPool(workerThreads);
-
     startConsumer();
   }
 
@@ -56,16 +53,6 @@ public final class JetQExecutor {
 
   public void shutdown() {
     shutdown = true;
-    executor.shutdown();
-  }
-
-  private void validateParameters(int queueSizePowerOfTwo, int workerThreads) {
-    if (queueSizePowerOfTwo < 0)
-      throw new IllegalArgumentException("Queue size power cannot be negative");
-    if (queueSizePowerOfTwo > 32)
-      throw new IllegalArgumentException("Queue size power too large (max 32)");
-    if (workerThreads < 1)
-      throw new IllegalArgumentException("Worker threads must be positive");
   }
 
   private void startConsumer() {
@@ -74,7 +61,7 @@ public final class JetQExecutor {
         TaskWrapper<?> task = queue.poll();
 
         if (task != null)
-          executor.execute(() -> task.execute());
+          Thread.startVirtualThread(() -> task.execute());
         else
           LockSupport.parkNanos(1_000);
       }
