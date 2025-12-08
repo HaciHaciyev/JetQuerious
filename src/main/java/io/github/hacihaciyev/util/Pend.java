055a6018c7d9bdaf9bd9f93b7968fdd5fd244fe0
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-12-09 02:44:52 +0400

    feat: new version of future container

---
+++ src/main/java/io/github/hacihaciyev/util/Pend.java
@@ -0,0 +1,46 @@
+package io.github.hacihaciyev.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+public final class Pend<T, E extends Exception> {
+    private final AtomicReference<Result<T,E>> result;
+    private final AtomicReference<Consumer<Result<T,E>>> callback;
+
+    public Pend() {
+        result = new AtomicReference<>(null);
+        callback = new AtomicReference<>(null);
+    }
+
+    public void completeOk(T value) {
+        complete(new Ok<>(value));
+    }
+
+    public void completeErr(E error) {
+        complete(new Err<>(error));
+    }
+
+    public void onComplete(Consumer<Result<T,E>> cb) {
+        Result<T,E> r = result.get();
+
+        if (r != null) {
+            cb.accept(r);
+            return;
+        }
+
+        if (callback.compareAndSet(null, cb)) {
+            r = result.get();
+            if (r != null) cb.accept(r);
+            return;
+        }
+
+        throw new IllegalStateException("Callback already set");
+    }
+
+    private void complete(Result<T,E> res) {
+        if (!result.compareAndSet(null, res)) return;
+
+        Consumer<Result<T,E>> cb = callback.get();
+        if (cb != null) cb.accept(res);
+    }
+}
