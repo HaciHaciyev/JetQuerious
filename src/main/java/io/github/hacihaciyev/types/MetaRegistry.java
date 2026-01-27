e5ad7ecffc929ee0bd24b793e8396737a2bc5268
HaciHaciyev <gadzievgadzi508@gmail.com>
2026-01-27 04:42:58 +0400

    Revert "feat: factory methods for static record creation through constructor"
    This reverts commit fc07d1e123d1752cd5decc7da88af12fefa36df4.

--- src/main/java/io/github/hacihaciyev/types/MetaRegistry.java
+++ src/main/java/io/github/hacihaciyev/types/MetaRegistry.java
@@ -9,17 +9,12 @@ public final class MetaRegistry {
     public sealed interface TypeMeta {
         None NONE = new None();
 
-        record Record<T>(Class<T> type, Field<T, ?>[] fields, RecordFactory<T> factory) implements TypeMeta {}
+        record Record<T>(Class<T> type, Field<T, ?>[] fields) implements TypeMeta {}
 
         record None() implements TypeMeta {}
     }
 
     public record Field<T, V>(String name, Class<V> type, Function<T, V> accessor) {}
-
-    @FunctionalInterface
-    public interface RecordFactory<T> {
-        T create(Object[] args);
-    }
 
     public static TypeMeta meta(Class<?> type) {
 
