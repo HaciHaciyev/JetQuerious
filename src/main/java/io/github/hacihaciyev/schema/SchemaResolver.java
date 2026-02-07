27bc7071b9d1e5aa6ae0df327c4c695c18a406ff
HaciHaciyev <gadzievgadzi508@gmail.com>
2026-02-07 05:19:39 +0400

    refactor: better messages

---
+++ src/main/java/io/github/hacihaciyev/schema/SchemaResolver.java
@@ -0,0 +1,133 @@
+package io.github.hacihaciyev.schema;
+
+import io.github.hacihaciyev.dsl.TableRef;
+import io.github.hacihaciyev.types.SQLType;
+import io.github.hacihaciyev.util.Err;
+import io.github.hacihaciyev.util.Ok;
+import io.github.hacihaciyev.util.Result;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static io.github.hacihaciyev.schema.Table.Catalog;
+import static io.github.hacihaciyev.schema.Table.Schema;
+
+// TODO feat cache
+public class SchemaResolver {
+
+    static final String TABLE_NOT_FOUND = "Table not found: catalog{%s}, schema{%s}, table{%s}";
+
+    private record CachedTable(Table table, long loadedAt) {}
+
+    private enum Meta {
+        TABLE_CAT,
+        TABLE_SCHEM,
+        TABLE_NAME,
+        COLUMN_NAME,
+        TYPE_NAME,
+        NULLABLE;
+
+        @Override
+        public String toString() {
+            return name();
+        }
+    }
+
+    private SchemaResolver() {}
+
+    public static Result<Table, SchemaVerificationException> load(TableRef tableRef, DataSource dataSource) {
+        requireNonNull(tableRef, "TableRef cannot be null");
+        requireNonNull(dataSource, "DataSource cannot be null");
+
+        var cat = catalogOrNull(tableRef);
+        var schema = schemaOrNull(tableRef);
+        var table = tableRef.name();
+        var types = new String[]{"TABLE", "VIEW"};
+
+        try (Connection conn = dataSource.getConnection()) {
+            var meta = conn.getMetaData();
+            return table(meta, cat, schema, table, types);
+        } catch (SQLException e) {
+            return new Err<>(new SchemaVerificationException(TABLE_NOT_FOUND.formatted(cat, schema, table), e));
+        }
+    }
+
+    private static Result<Table, SchemaVerificationException> table(
+            DatabaseMetaData meta, String cat, String schema, String table, String[] types) throws SQLException {
+
+        try (var tables = meta.getTables(cat, schema, table, types)) {
+            if (!tables.next()) return new Err<>(new SchemaVerificationException(TABLE_NOT_FOUND.formatted(cat, schema, table)));
+
+            var actualCat = cat(tables);
+            var actualSchem = schem(tables);
+            var columns = loadColumns(meta, actualCat, actualSchem, table);
+
+            if (columns.length == 0)
+                return new Err<>(new SchemaVerificationException("Table has no accessible columns: " + table));
+
+            return new Ok<>(new Table(actualCat, actualSchem, table, columns));
+        }
+    }
+
+    private static Catalog cat(ResultSet tables) throws SQLException {
+        var cat = tables.getString(Meta.TABLE_CAT.toString());
+        if (cat == null) return new Catalog.Unknown();
+        return new Catalog.Known(cat);
+    }
+
+    private static Schema schem(ResultSet tables) throws SQLException {
+        var schem = tables.getString(Meta.TABLE_SCHEM.toString());
+        if (schem == null) return new Schema.Unknown();
+        return new Schema.Known(schem);
+    }
+
+    private static Column[] loadColumns(
+            DatabaseMetaData metaData, Catalog catalog, Schema schema, String table) throws SQLException {
+
+        var cols = new ArrayList<Column>();
+
+        try (var rs = metaData.getColumns(asStrOrNull(catalog), asStrOrNull(schema), table, null)) {
+            while (rs.next()) column(rs, cols);
+        }
+
+        return cols.toArray(new Column[0]);
+    }
+
+    private static void column(ResultSet rs, List<Column> cols) throws SQLException {
+        var name = rs.getString(Meta.COLUMN_NAME.toString());
+        var type = rs.getString(Meta.TYPE_NAME.toString());
+        var nullable = rs.getInt(Meta.NULLABLE.toString()) == DatabaseMetaData.columnNullable;
+
+        SQLType.parse(type).ifPresentOrElse(
+                t -> cols.add(new Column.Known(name, t, nullable)),
+                () -> cols.add(new Column.Unknown(name, nullable))
+        );
+    }
+
+    private static String schemaOrNull(TableRef tableRef) {
+        if (tableRef instanceof TableRef.SchemaRef sr) return sr.schema();
+        return null;
+    }
+
+    private static String catalogOrNull(TableRef tableRef) {
+        if (tableRef instanceof TableRef.CatalogRef cr) return cr.catalog();
+        return null;
+    }
+
+    private static String asStrOrNull(Catalog catalog) {
+        if (catalog instanceof Catalog.Known(var name)) return name;
+        return null;
+    }
+
+    private static String asStrOrNull(Schema schema) {
+        if (schema instanceof Schema.Known(var name)) return name;
+        return null;
+    }
+}
