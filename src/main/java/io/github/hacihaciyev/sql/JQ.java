529f46505af08702a98e352a2a06fdc4f138fb35
HaciHaciyev <gadzievgadzi508@gmail.com>
2026-04-29 13:18:42 +0400

    feat: DQL & DML contexts for JQ

--- src/main/java/io/github/hacihaciyev/sql/JQ.java
+++ src/main/java/io/github/hacihaciyev/sql/JQ.java
@@ -1,13 +1,24 @@
 package io.github.hacihaciyev.sql;
 
+import io.github.hacihaciyev.sql.internal.value_objects.DML;
+import io.github.hacihaciyev.sql.internal.value_objects.DQL;
+
+import static java.util.Objects.requireNonNull;
+
 public sealed interface JQ {
     String sql();
     
-    record Read(String sql) implements JQ {
-        
+    record Read(String sql, DQL context) implements JQ {
+        public Read {
+            requireNonNull(sql);
+            requireNonNull(context);
+        }
     }
     
-    record Write(String sql) implements JQ {
-        
+    record Write(String sql, DML context) implements JQ {
+        public Write {
+            requireNonNull(sql);
+            requireNonNull(context);
+        }
     }
 }
